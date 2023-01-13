ee26e1fdeb1ac473ac807e771327f4d154609543
Sven Notermans <sven.notermans@live.nl>
2023-01-13 14:21:26 +0100

    added component VacationPicker

---
+++ src/data/countryData.js
@@ -0,0 +1,22 @@
+const countryData = {
+  countries: [
+    {
+      id: 1,
+      name: "USA",
+      capital: 'Washington DC',
+      cost: 1250,
+      details: 'United States are among the most visited (...)',
+      img: 'https://media.istockphoto.com/id/1289724591/photo/aerial-view-of-the-washington-dc-at-night.jpg?s=612x612&w=0&k=20&c=jDvoxjN597_ysSZ5AvuVtdlFd6Gpqr9_PxdIIYLJdlc=',
+    },
+    {
+      id: 2,
+      name: "Netherlands",
+      capital: 'Amsterdam',
+      cost: 750,
+      details: 'Capitol of the Netherlands is Amsterdam',
+      img: 'https://cdn.pixabay.com/photo/2016/01/19/19/26/amsterdam-1150319__480.jpg',
+    },
+
+  ]
+}
+export default countryData;
